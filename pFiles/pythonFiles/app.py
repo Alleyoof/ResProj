2cebfbf06badf83c1c31390fbf22cb3789df2c92
Jonathan <vpjonathan109@gmail.com>
2023-01-11 13:31:15 -0500

    deleted folder

--- pFiles/pythonFiles/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__) # name for the Flask app (refer to output)
-# running the server
-@app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
-def home(): 
-    return "Hello world!"
-if __name__ == "__main__":
-    app.run(debug = True)
